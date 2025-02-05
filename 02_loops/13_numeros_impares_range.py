db53e11be0b6e7f6f0808a0dbc67133d064ffb8c
AleMartinez96 <xalpha96710@gmail.com>
2025-02-05 16:04:13 -0300

    add range

---
+++ 02_loops/13_numeros_impares_range.py
@@ -0,0 +1,6 @@
+# Ejercicio 13: Imprimir números impares del 1 al 20
+# Imprime todos los números impares entre 1 y 20 (inclusive) usando un bucle for y range().
+
+for i in range(1, 21):
+    if i % 2 != 0:
+        print(i)
---
+++ 02_loops/14_multiplo_5_range.py
@@ -0,0 +1,6 @@
+# Ejercicio 3: Imprimir múltiplos de 5
+# Imprime los múltiplos de 5 desde 5 hasta 50 (inclusive) usando un bucle for y range().
+
+for i in range(5, 51):
+    if i % 5 == 0:
+        print(i)
